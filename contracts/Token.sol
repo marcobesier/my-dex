ab1f0fff4f4a70a3b2e952ec8a39380b9e4dfa85
Marco Besier <marcobesier@icloud.com>
2023-01-18 09:10:19 +0100

    Add transfer function

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,6 +10,12 @@ contract Token {
     // Track account balances
     mapping(address => uint) public balanceOf;
 
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+
     constructor(
         string memory _name,
         string memory _symbol,
@@ -22,4 +28,25 @@ contract Token {
         totalSupply = _totalSupply * (10 ** decimals);
         balanceOf[msg.sender] = totalSupply;
     }
+
+    function transfer(address _to, uint _value) public returns (bool success) {
+        
+        require(
+            _to != address(0),
+            "Transferring to zero address is not permitted"
+        );
+
+        require(
+            balanceOf[msg.sender] >= _value,
+            "Insufficient funds"
+        );
+        
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
 }
--- test/Token.js
+++ test/Token.js
@@ -7,7 +7,7 @@ const tokens = (n) => {
 
 describe("Token", () => {
 
-  let token, accounts, deployer
+  let token, accounts, deployer, receiver
 
   beforeEach(async () => {
     const Token = await ethers.getContractFactory("Token")
@@ -15,6 +15,7 @@ describe("Token", () => {
 
     accounts = await ethers.getSigners()
     deployer = accounts[0]
+    receiver = accounts[1]
   })
 
   describe("Deployment", () => {
@@ -44,6 +45,54 @@ describe("Token", () => {
       expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
     })
 
+  })
+
+  describe("Sending Tokens", () => {
+
+    let amount, transaction, receipt
+
+    describe("Successful Transfers", () => {
+
+      beforeEach(async () => {
+        amount = tokens(100)
+        transaction = await token.connect(deployer).transfer(receiver.address, amount)
+        receipt = await transaction.wait()
+      })
+  
+      it("Transfers tokens", async () => {
+        expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+        expect(await token.balanceOf(receiver.address)).to.equal(amount)
+      })
+  
+      it("Emits a Transfer event", async () => {
+        const event = receipt.events[0]
+        expect(event.event).to.equal("Transfer")
+  
+        const args = event.args
+        expect(args._from).to.equal(deployer.address)
+        expect(args._to).to.equal(receiver.address)
+        expect(args._value).to.equal(amount)
+      })
+
+    })
+
+    describe("Failing Transfers", () => {
+      
+      it("Rejects transfer if sender doesn't have sufficient funds", async () => {
+        // The receiver account has no tokens. Thus, trying to send 10 should be reverted.
+        const invalidAmount = tokens(10)
+        await expect(token.connect(receiver).transfer(deployer.address, invalidAmount)).to.be.revertedWith("Insufficient funds")
+      })
+
+      it("Rejects transfer if receiver is the zero address", async () => {
+        const amount = tokens(10)
+        await expect(token.connect(deployer).transfer("0x0000000000000000000000000000000000000000", amount)).to.be.revertedWith("Transferring to zero address is not permitted")
+      })
+
+    })
+
+    
+  
   })
 
 })
