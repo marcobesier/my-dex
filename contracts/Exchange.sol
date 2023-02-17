d479ed8111a660cdf577ed47ff2dc4de10517682
Marco Besier <marcobesier@icloud.com>
2023-02-17 09:50:39 +0100

    Implement depositToken

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,12 +1,34 @@
 // SPDX-License-Identifier: UNLICENSE
 pragma solidity ^0.8.0;
 
+import "./Token.sol";
+
 contract Exchange {
     address public feeAccount;
     uint public feePercent;
 
+    mapping(address => mapping(address => uint)) public tokens;
+
+    event Deposit(
+        address _token,
+        address _user,
+        uint _amount,
+        uint _balance
+    );
+
     constructor(address _feeAccount, uint _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+    }
+
+    function depositToken(address _token, uint _amount) public {
+        Token(_token).transferFrom(msg.sender, address(this), _amount);
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
+
+        emit Deposit(_token, msg.sender, _amount, balanceOf(_token, msg.sender));
+    }
+
+    function balanceOf(address _token, address _user) public view returns (uint) {
+        return tokens[_token][_user];
     }
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe("Exchange", () => {
-  let deployer, feeAccount, exchange
+  let deployer, feeAccount, exchange, token1, user1
 
   const feePercent = 10
 
@@ -14,9 +14,15 @@ describe("Exchange", () => {
     const accounts = await ethers.getSigners()
     deployer = accounts[0]
     feeAccount = accounts[1]
+    user1 = accounts[2]
 
     const Exchange = await ethers.getContractFactory("Exchange")
     exchange = await Exchange.deploy(feeAccount.address, feePercent)
+
+    const Token = await ethers.getContractFactory("Token")
+    token1 = await Token.deploy("My Token", "MT", 1000000)
+
+    await token1.connect(deployer).transfer(user1.address, tokens(100))
   })
 
   describe("Deployment", () => {
@@ -27,6 +33,41 @@ describe("Exchange", () => {
 
     it("Tracks the fee percent", async () => {
       expect(await exchange.feePercent()).to.equal(feePercent)
+    })
+
+  })
+
+  describe("Depositing Tokens", () => {
+
+    let receipt
+    let amount = tokens(10)
+
+    beforeEach(async () => {
+      // Approve tokens
+      await token1.connect(user1).approve(exchange.address, amount)
+      // Deposit tokens
+      const transaction = await exchange.connect(user1).depositToken(token1.address, amount)
+      receipt = await transaction.wait()
+    })
+
+    it("Tracks the token deposit", async () => {
+      // Ensure the tokens were transferred to the exchange
+      expect(await token1.balanceOf(exchange.address)).to.equal(amount)
+      expect(await token1.balanceOf(user1.address)).to.equal(tokens(90))
+      // Ensure exchange keeps track of the deposits
+      expect(await exchange.tokens(token1.address, user1.address)).to.equal(amount)
+      expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(amount)
+    })
+
+    it("Emits a Deposit event", async () => {
+      const event = receipt.events[1]
+      expect(event.event).to.equal("Deposit")
+
+      const args = event.args
+      expect(args._token).to.equal(token1.address)
+      expect(args._user).to.equal(user1.address)
+      expect(args._amount).to.equal(amount)
+      expect(args._balance).to.equal(amount)
     })
 
   })
