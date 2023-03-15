c8e16d6a22f874625daf74becc73d67203417221
Marco Besier <marcobesier@icloud.com>
2023-03-15 13:45:57 +0100

    Implement seed exchange script

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,12 +1,29 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  // Fetch contract to deploy
+  console.log("Preparing deployment...\n")
+  
+  // Fetch contracts to deploy
   const Token = await ethers.getContractFactory("Token")
+  const Exchange = await ethers.getContractFactory("Exchange")
 
-  // Deploy the contract
-  const token = await Token.deploy()
-  console.log(`Token deployed at: ${token.address}`)
+  // Fetch accounts
+  const accounts = await ethers.getSigners()
+
+  console.log(`Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`)
+
+  // Deploy the contracts
+  const MT = await Token.deploy("My Token", "MT", "1000000")
+  console.log(`MT deployed at: ${MT.address}`)
+
+  const mETH = await Token.deploy("Mock ETH", "mETH", "1000000")
+  console.log(`mETH deployed at: ${mETH.address}`)
+
+  const mDAI = await Token.deploy("Mock DAI", "mDAI", "1000000")
+  console.log(`mDAI deployed at: ${mDAI.address}`)
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  console.log(`Exchange deployed at: ${exchange.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
