d4c6cc87dcc090d6276846d021981ee6113e1549
Marco Besier <marcobesier@icloud.com>
2023-04-27 13:26:11 +0200

    Put finishing touches on Redux setup

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import {
   loadProvider, 
   loadNetwork, 
   loadAccount,
-  loadToken 
+  loadTokens,
+  loadExchange,
 } from '../store/interactions.js';
 
 function App() {
@@ -13,14 +14,21 @@ function App() {
   const dispatch = useDispatch()
 
   const loadBlockchainData = async () => {
-    await loadAccount(dispatch)
 
     // Connect to blockchain
     const provider = loadProvider(dispatch)
     const chainId = await loadNetwork(provider, dispatch)
 
+    await loadAccount(provider, dispatch)
+
     // Token smart contract
-    await loadToken(provider, config[chainId].MT.address, dispatch)
+    const MT = config[chainId].MT.address
+    const mETH = config[chainId].mETH.address
+    await loadTokens(provider, [MT, mETH], dispatch)
+
+    // Echange contract
+    const exchange = config[chainId].exchange.address
+    await loadExchange(provider, exchange, dispatch)
   }
 
   useEffect(() => {
